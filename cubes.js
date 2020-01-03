d260a58bbbe1b4e93ac28b31c843332a97e5ba66
Zlatoslav <z.marchev@abv.bg>
2020-01-03 23:34:11 +0200

    Noise

--- cubes.js
+++ cubes.js
@@ -4,6 +4,7 @@ const random = require('canvas-sketch-util/random');
 const palettes = require('nice-color-palettes');
 const eases = require('eases');
 const BezierEasing = require('bezier-easing');
+const glsl = require('glslify');
 
 // Include any additional ThreeJS examples below
 require("three/examples/js/controls/OrbitControls");
@@ -11,7 +12,7 @@ require("three/examples/js/controls/OrbitControls");
 const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [512, 512],
+  dimensions: [590, 590],
   fps: 24,
   duration: 8,
   // Make the loop animated
@@ -39,16 +40,45 @@ const sketch = ({ context }) => {
 
   const palette = random.pick(palettes);
 
+  const fragmentShader = `
+      varying vec2 vUv;
+
+      uniform vec3 color;
+
+      void main() {
+        gl_FragColor = vec4(vec3(color * vUv.x), 1.0);
+      }
+  `;
+
+  const vertexShader = glsl(`
+        varying vec2 vUv;
+        uniform float time;
+
+        #pragma glslify: noise = require('glsl-noise/simplex/4d');
+
+        void main() {
+          vUv = uv;
+          vec3 pos = position.xyz;
+          pos += noise(vec4(position.xyz, time)) * 1.0;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+        }
+      `);
+
   // Setup a geometry
   const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-
+  const meshes = [];
   // Setup a mesh with geometry + material
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 40; i++) {
     const mesh = new THREE.Mesh(
       geometry,
-      new THREE.MeshStandardMaterial({
-        color: random.pick(palette),
+      new THREE.ShaderMaterial({
+        fragmentShader,
+        vertexShader,
+        uniforms: {
+          color: { value: new THREE.Color(random.pick(palette)) },
+          time: { value: 0 }
+        }
       }));
     mesh.position.set(
       random.range(-1, 1),
@@ -64,6 +94,7 @@ const sketch = ({ context }) => {
     mesh.scale.multiplyScalar(0.5);
 
     scene.add(mesh);
+    meshes.push(mesh);
   }
 
   scene.add(new THREE.AmbientLight('hsl(0, 0%, 40%)'));
@@ -107,9 +138,14 @@ const sketch = ({ context }) => {
 
     },
     // Update & render your scene here
-    render({ playhead }) {
+    render({ playhead, time }) {
       const t = Math.sin(playhead * Math.PI * 2) * 2;
       scene.rotation.z = easeFn(t);
+
+      meshes.forEach(mesh => {
+        mesh.material.uniforms.time.value = time;
+      })
+
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
--- shader.js
+++ shader.js
@@ -16,19 +16,33 @@ const frag = glsl(`
   uniform float aspect;
   varying vec2 vUv;
 
+  #pragma glslify: noise = require('glsl-noise/simplex/3d');
+  #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
+
   void main () {
-    vec3 colorA = sin(time * 1.0) + vec3(1.0, 0.0, 0.0);
-    vec3 colorB = vec3(0.0, 1.0, 0.0);
+    // vec3 colorA = sin(time * 1.0) + vec3(1.0, 0.0, 0.0);
+    // vec3 colorB = vec3(0.0, 1.0, 0.0);
 
     vec2 center = vUv - 0.5;
     center.x *= aspect;
 
     float dist = length(center);
 
-    float alpha = smoothstep(0.75 + sin(time), 0.25, dist);
+    float alpha = smoothstep(0.25, 0.2475, dist);
 
-    vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
+    // vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
+    // gl_FragColor = vec4(color, alpha);
+
+    float n = noise(vec3(center * 1.0, time));
+
+    vec3 color = hsl2rgb(
+      0.5 + n * 0.2,
+      0.5,
+      0.5
+    );
+
     gl_FragColor = vec4(color, alpha);
+
   }
 `);
 
